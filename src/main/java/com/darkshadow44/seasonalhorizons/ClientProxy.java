b015f2fc99607347f5c558851b83905df199bdb3
Fabian Maurer <dark.shadow4@web.de>
2025-04-24 15:41:01 +0200

    Init

---
+++ src/main/java/com/darkshadow44/seasonalhorizons/ClientProxy.java
@@ -0,0 +1,5 @@
+package com.darkshadow44.seasonalhorizons;
+
+public class ClientProxy extends CommonProxy {
+
+}
---
+++ src/main/java/com/darkshadow44/seasonalhorizons/asm/SeasonalHorizonsAsmCore.java
@@ -0,0 +1,4 @@
+package com.darkshadow44.seasonalhorizons.asm;
+
+public class SeasonalHorizonsAsmCore {
+}
